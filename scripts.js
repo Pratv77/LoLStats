ce40399a7fa075c8d031d72733de80f818a7ef9c
Pratham <pratv7@outlook.com>
2022-12-21 20:29:46 -0500

    got rid of flashy title and searchbar animations

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function redirect(event) {
+
+  event.preventDefault();
+
+  let username = document.getElementById('mysearch');
+  console.log(username.value)
+  // let url = 'index2.html?q=' + username;
+
+  // window.location.href = url;
+
+
+}
