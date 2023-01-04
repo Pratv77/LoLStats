dce44f01ccf2a5e144aa3b387295607de689eda6
Pratham <pratv7@outlook.com>
2023-01-04 18:22:53 -0500

    finally fixed form get method

--- scripts2.js
+++ scripts2.js
@@ -1,4 +1,8 @@
 const apiKey = "RGAPI-6e9d7c84-4dd4-4b4a-b2fb-a2b6a3e153d7";
 
+// Get player's username from search
+const urlValue = window.location.search;
+const urlParams = new URLSearchParams(urlValue)
 
-// console.log(document.getElementById("write").innerHTML = window.location.search)
+const username = urlParams.get('username')
+
