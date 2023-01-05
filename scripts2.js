cbea3849c6d184f49ee1bb5c9e43725cdd773b27
Pratham <pratv7@outlook.com>
2023-01-05 16:46:19 -0500

    Works perfectly, just need to add more stats to display and then style it

--- scripts2.js
+++ scripts2.js
@@ -1,4 +1,4 @@
-const apiKey = "RGAPI-5f02c47f-6b48-4377-b82d-008e2bd92af1";
+const apiKey = "RGAPI-be52d723-24ba-40c4-85f6-6226df3e3b10";
 
 // Get player's username from search
 const urlValue = window.location.search;
@@ -6,20 +6,22 @@ const urlParams = new URLSearchParams(urlValue);
 
 const username = urlParams.get("username");
 
-function fetchLeague(username) {
-  fetch(
-    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}` // NA region only right now
-  )
-    .then((response) => response.json())
-    .then((data) => this.displayData(data));
+fetchLeague(username);
+
+async function fetchLeague(username) {
+  const response = await fetch(
+    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}`
+  );
+  const data = await response.json();
+  displayData(data);
 }
 
 function displayData(data) {
   const { name, profileIconId, summonerLevel } = data;
 
   document.querySelector(".name").innerHTML = name;
-  // document.querySelector('.profile__img').src = ;   add profile icon id to get profileicon
+  document.querySelector('.profile__img').src = `http://ddragon.leagueoflegends.com/cdn/12.23.1/img/profileicon/${profileIconId}.png`;
   document.querySelector(".level").innerHTML = "Level: " + summonerLevel;
 }
 
-fetchLeague(username);
+
