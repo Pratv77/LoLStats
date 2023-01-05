d5beecdc9563caa5c6b22ef3afe4a241340a09f7
Pratham <pratv7@outlook.com>
2023-01-05 15:27:45 -0500

    made API calls

--- scripts2.js
+++ scripts2.js
@@ -1,8 +1,25 @@
-const apiKey = "RGAPI-6e9d7c84-4dd4-4b4a-b2fb-a2b6a3e153d7";
+const apiKey = "RGAPI-5f02c47f-6b48-4377-b82d-008e2bd92af1";
 
 // Get player's username from search
 const urlValue = window.location.search;
-const urlParams = new URLSearchParams(urlValue)
+const urlParams = new URLSearchParams(urlValue);
 
-const username = urlParams.get('username')
+const username = urlParams.get("username");
 
+function fetchLeague(username) {
+  fetch(
+    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}` // NA region only right now
+  )
+    .then((response) => response.json())
+    .then((data) => this.displayData(data));
+}
+
+function displayData(data) {
+  const { name, profileIconId, summonerLevel } = data;
+
+  document.querySelector(".name").innerHTML = name;
+  // document.querySelector('.profile__img').src = ;   add profile icon id to get profileicon
+  document.querySelector(".level").innerHTML = "Level: " + summonerLevel;
+}
+
+fetchLeague(username);
