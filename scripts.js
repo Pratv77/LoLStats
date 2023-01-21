1f80017db17b64a6460f90733c216debeed2207d
Pratham <pratv7@outlook.com>
2023-01-21 00:56:16 -0500

    changed index.html

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 function redirect() {
   // Get the value of the input box
   var input = document.getElementById("input").value;
-  
+
   // Construct the URL with the input value as a query parameter
   var url = "./index2.html" + input;
-  
+
   // Redirect to the URL
   window.location.href = url;
 }
